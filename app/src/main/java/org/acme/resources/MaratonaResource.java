8d1161f946a446c851c12ba4991738c33a6c3964
jasondavindev <jasondavin.dev@gmail.com>
2019-11-11 21:43:56 -0300

    finish endpoin implementation

--- app/src/main/java/org/acme/resources/MaratonaResource.java
+++ app/src/main/java/org/acme/resources/MaratonaResource.java
@@ -1,38 +1,53 @@
 package org.acme.resources;
 
 import java.util.Set;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-
+import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
+import org.acme.controllers.MaratonaController;
 import org.acme.models.Solution;
-import org.acme.services.ExecutionService;
-import org.acme.services.ValidationService;
 
 @Path("/maratona")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class MaratonaResource {
-	private Set<Solution> solutions = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+	private MaratonaController controller;
+
+	public MaratonaResource() {
+		controller = new MaratonaController();
+	}
 
 	@POST
 	public Set<Solution> save(Solution solution) {
-		ExecutionService.executeScript(solution);
-		boolean isValid = ValidationService.runValidation(solution);
-		solution.setStatus(isValid ? "SUCCESS" : "FAIL");
-		solutions.add(solution);
-		return solutions;
+		return controller.save(solution);
+	}
+
+	@GET
+	@Path("/status/{status}")
+	public List<Solution> filterByStatus(@PathParam("status") String status) {
+		return controller.filterByStatus(status);
+	}
+
+	@GET
+	@Path("/problem/{problem}")
+	public List<Solution> filterByProblem(@PathParam("problem") String problem) {
+		return controller.filterByProblem(problem);
+	}
+
+	@GET
+	@Path("/date/{datetime}")
+	public List<Solution> filterByDatetime(@PathParam("datetime") long datetime) {
+		return controller.filterByDatetime(datetime);
 	}
 
 	@GET
 	public Set<Solution> show() {
-		return solutions;
+		return controller.getSolutions();
 	}
 }
